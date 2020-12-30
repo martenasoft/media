222006e31072c66f11d94b380421a2130fd72c4a
kanisimov <kanisimov@xmlshop.com>
2020-12-30 18:06:52 +0200

    Common Directory Manager Service

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,44 +14,7 @@ class Configuration implements ConfigurationInterface
 
         $treeBuilder->getRootNode()
             ->children()
-            ->arrayNode('images_path')
-            ->defaultValue([
-                [
-                    'name' => null,
-                    "real_path" => null,
-                    "big_path" => null,
-                    "middle_path" => null,
-                    "small_path" => null,
-                    "big_width" => null,
-                    "big_height" => null,
-                    "middle_width" => null,
-                    "middle_height" => null,
-                    "small_width" => null,
-                    "small_height" => null,
-
-                    "big_watermark" => null,
-                    "big_watermark_path" => null,
-                    "big_watermark_top" => null,
-                    "big_watermark_left" => null,
-
-                    "middle_watermark" => null,
-                    "middle_watermark_path" => null,
-                    "middle_watermark_top" => null,
-                    "middle_watermark_left" => null,
-
-                    "small_watermark" => null,
-                    "small_watermark_path" => null,
-                    "small_watermark_top" => null,
-                    "small_watermark_left" => null,
-
-                    "cdn_url" => null,
-                    "cdn_dev_url" => null
-                ]
-            ])
-            ->prototype("scalar")
-            ->end()
-            ->end()
-            ->end()
+            ->scalarNode('tmp_dir')->defaultValue('var/cache/uploader')
             ->end();
         return $treeBuilder;
     }
--- src/Service/UploadImageService.php
+++ src/Service/UploadImageService.php
@@ -3,23 +3,34 @@
 namespace MartenaSoft\Media\Service;
 
 use MartenaSoft\Common\Exception\CommonException;
+use MartenaSoft\Common\Service\DirectoryManagerService\DirectoryManagerService;
 use MartenaSoft\Media\Entity\MediaConfig;
+use MartenaSoft\Media\MartenaSoftMediaBundle;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
-use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class UploadImageService
 {
     private string $targetDirectory;
     private SluggerInterface $slugger;
     private KernelInterface $kernel;
+    private LoggerInterface $logger;
+    private DirectoryManagerService $directoryManagerService;
 
-    public function __construct(SluggerInterface $slugger, KernelInterface $kernel)
+    public function __construct(
+        SluggerInterface $slugger,
+        KernelInterface $kernel,
+        LoggerInterface $logger,
+        DirectoryManagerService $directoryManagerService
+    )
     {
         $this->slugger = $slugger;
         $this->kernel = $kernel;
+        $this->logger = $logger;
+        $this->directoryManagerService = $directoryManagerService;
     }
 
     public function upload(
@@ -28,26 +39,46 @@ class UploadImageService
         string $publicDir = 'public',
         ?string $projectDir = null
     ): string {
+
+        $parameters = $this->kernel->getContainer()->getParameter(MartenaSoftMediaBundle::getConfigName());
+
+        $templateDirectory = $parameters['tmp_dir'];
+
+        $this->directoryManagerService->createDirByPath($templateDirectory);
+
+        die("file: ".__FILE__ ." line: ".__LINE__);
+
         $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
         $fileName = $safeFilename. '-' . uniqid().'.'.$uploadedFile->guessExtension();
 
-        $realSizeDir = $this->getPath($mediaConfig->getRealSizeDirName().'//', $publicDir, $projectDir);
-
-        if ($realSizeDir !== null) {
-
-        }
-
-        dump($projectDir, $mediaConfig); die;
         try {
-            if (empty($mediaConfig)) {
-                throw \Exception('This functional is in developing process');
-               // $uploadedFile->move($this->getTargetDirectory(), $fileName);
+          //  $mediaConfig->setRealSizeDirName('img/article/real');
+            $realSizeDir = $this->getPath($mediaConfig->getRealSizeDirName(), $publicDir, $projectDir);
+
+            if ($realSizeDir !== null) {
+
+            } else {
+
+                $this->logger->warning("sdsdsd");
+                dump($templateDirectory);
             }
 
-        } catch (FileException $exception) {
+          //  die("file: ".__FILE__ ." line: ".__LINE__);
+          /*  if (empty($mediaConfig)) {
+
+
+                throw \Exception('This functional is in developing process');
+               // $uploadedFile->move($this->getTargetDirectory(), $fileName);
+            }*/
+
+
+        } catch (CommonException | FileException | \Throwable $exception) {
+
             throw $exception;
         }
+
+        return $fileName;
     }
 
 
@@ -62,12 +93,12 @@ class UploadImageService
     }
 
     protected function getPath(
-        string $path,
+        ?string $path = null,
         string $publicDir = 'public',
         ?string $projectDir = null
     ): ?string {
 
-        if (empty($this->kernel->getProjectDir())) {
+        if (empty($path) || empty($this->kernel->getProjectDir())) {
             return null;
         }
 
@@ -81,9 +112,12 @@ class UploadImageService
         $result = preg_replace( '/(\/{2,})/', '/', $projectDir . DIRECTORY_SEPARATOR . $path);
         $result = preg_replace('/\/{1,}$/', '', $result);
         if (!is_writable($result)) {
-            throw new NotFoundResourceException('Directory: '.$result . ' is not available');
+            $commonException = new CommonException('Directory: '.$result . ' is not available');
+            $commonException
+                ->setUserMessage('Directory: '
+                    . $publicDir. DIRECTORY_SEPARATOR . $path . ' is not available');
+            throw $commonException;
         }
         return $result;
     }
-
 }
