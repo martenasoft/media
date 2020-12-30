793543c3269f33b6894bbba820ddb801b7dcc821
primawebsystems <kostiaGm@gmail.com>
2020-12-30 12:55:41 +0200

    Upload image service. Added get bathe path method

--- src/Service/UploadImageService.php
+++ src/Service/UploadImageService.php
@@ -2,28 +2,43 @@
 
 namespace MartenaSoft\Media\Service;
 
+use MartenaSoft\Common\Exception\CommonException;
 use MartenaSoft\Media\Entity\MediaConfig;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class UploadImageService
 {
     private string $targetDirectory;
     private SluggerInterface $slugger;
+    private KernelInterface $kernel;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, KernelInterface $kernel)
     {
         $this->slugger = $slugger;
+        $this->kernel = $kernel;
     }
 
-    public function upload(UploadedFile $uploadedFile, ?MediaConfig $mediaConfig = null): string
-    {
+    public function upload(
+        UploadedFile $uploadedFile,
+        ?MediaConfig $mediaConfig = null,
+        string $publicDir = 'public',
+        ?string $projectDir = null
+    ): string {
         $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
         $fileName = $safeFilename. '-' . uniqid().'.'.$uploadedFile->guessExtension();
 
-        dump($mediaConfig); die;
+        $realSizeDir = $this->getPath($mediaConfig->getRealSizeDirName().'//', $publicDir, $projectDir);
+
+        if ($realSizeDir !== null) {
+
+        }
+
+        dump($projectDir, $mediaConfig); die;
         try {
             if (empty($mediaConfig)) {
                 throw \Exception('This functional is in developing process');
@@ -45,4 +60,30 @@ class UploadImageService
     {
         return $this->targetDirectory;
     }
+
+    protected function getPath(
+        string $path,
+        string $publicDir = 'public',
+        ?string $projectDir = null
+    ): ?string {
+
+        if (empty($this->kernel->getProjectDir())) {
+            return null;
+        }
+
+        if ($projectDir === null) {
+            $projectDir = $this->kernel->getProjectDir() .
+                DIRECTORY_SEPARATOR .
+                $publicDir
+            ;
+        }
+
+        $result = preg_replace( '/(\/{2,})/', '/', $projectDir . DIRECTORY_SEPARATOR . $path);
+        $result = preg_replace('/\/{1,}$/', '', $result);
+        if (!is_writable($result)) {
+            throw new NotFoundResourceException('Directory: '.$result . ' is not available');
+        }
+        return $result;
+    }
+
 }
